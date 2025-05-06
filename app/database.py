2bf6924b90cf74b4e14d96b74b11248f3046204a
UFOTreeboy <jinn7431080@gmail.com>
2025-05-06 23:05:25 +0800

    完善資料庫讀取

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from pymongo.mongo_client import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class MyMongoDB:
+    def __init__(self):
+        self.mongodb_uri = os.environ.get('MONGODB_DATABASE_URL')
+        self.database_name = os.environ.get('DATABASE_NAME')
+
+        if not self.database_name:
+            raise ValueError("MongoDB URI沒有設置")
+
+        try:
+            self.client = MongoClient(self.mongodb_uri)
+            self.db = self.client[self.database_name]
+        except Exception as e:
+            raise ConnectionError(f"無法連接到 MongoDB: {str(e)}")
+
+    def connect_to_collection(self, collection_name):
+        return self.db[collection_name]
