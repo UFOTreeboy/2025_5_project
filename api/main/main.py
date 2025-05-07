82d8588ba1d79b3499cd2e786bb02f93d5f0b09b
UFOTreeboy <jinn7431080@gmail.com>
2025-05-07 21:43:11 +0800

    新增增刪查改的網頁

--- api/main/main.py
+++ api/main/main.py
@@ -1,45 +1,50 @@
-from flask import Blueprint, render_template, jsonify, request
+from flask import Blueprint, render_template, jsonify, request,redirect
 from api.database.database import MyMongoDB
 from uuid import uuid4
 from flask_wtf.csrf import CSRFProtect
 from bson import ObjectId
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__,
+                 template_folder= 'templates',)
+
 csrf = CSRFProtect()
 
-@main.route("/main", methods=['GET'])
-def render_main_html():
-    return render_template('main.html')
-
-@main.route("/user", methods=['GET', 'POST', 'PUT', 'DELETE'])
+@main.route("/main", methods=['GET', 'POST', 'PUT', 'DELETE'])
 @csrf.exempt
 def user_handler():
     mongo = MyMongoDB()
     collection = mongo.connect_to_collection("customer_list")
 
-
     try:
         if request.method == 'POST':
-            content = request.get_json()
+
+            if request.form:
+                content = request.form.to_dict()
+            else:
+                content = request.get_json()
+
             if not content:
                 return jsonify({"message": "Invalid input"}), 400
             content["_id"] = uuid4().hex
             collection.insert_one(content)
-            return jsonify({"message": "User added", "id": content["_id"]}), 201
 
+            if request.form:
+                return redirect("/main")
+
+            return jsonify({"message": "User added", "id": content["_id"]}), 201
 
         elif request.method == 'GET':
             users = collection.find({}, {'_id': 1, 'Contact Person': 1, 'Email': 1, 'Phone': 1, 'Address': 1})
             result = []
             for user in users:
                 user['_id'] = str(user['_id'])
-                user['Contact Person'] = user.get('Contact Person')
-                user['Email'] = user.get('Email')
-                user['Phone'] = user.get('Phone')
-                user['Address'] = user.get('Address')
+                user['Contact Person'] = user.get('Contact Person','None')
+                user['Email'] = user.get('Email','None')
+                user['Phone'] = user.get('Phone','None')
+                user['Address'] = user.get('Address','None')
 
                 result.append(user)
-            return jsonify({"data": result}), 200
+            return render_template('main.html', users=result)
         
 
         elif request.method == 'PUT':
@@ -57,7 +62,8 @@ def user_handler():
             result = collection.update_one({"_id": user_id}, {"$set": update_data})
             if result.matched_count == 0:
                 return jsonify({"message": "User not found"}), 404
-            return jsonify({"message": "User updated"}), 200
+            
+            return redirect("/main")
         
 
         elif request.method == 'DELETE':
@@ -72,7 +78,8 @@ def user_handler():
             result = collection.delete_one({"_id": user_id})
             if result.deleted_count == 0:
                 return jsonify({"message": "User not found"}), 404
-            return jsonify({"message": "User deleted"}), 200
+            
+            return redirect("/main")
         
 
     except Exception as e:
